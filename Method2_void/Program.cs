b23cb8b53ec7efdb97f4102cf210d3be9480055c
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-05 19:38:07 +0400

    added Method2 void

---
+++ Method2_void/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Вид 2. Ничего не взвращаяют но принимают какие то аргументы
+
+void Method2(string msg)    // тип данных / имя метода / () принятые аргументы
+{ 
+    Console.WriteLine(msg);
+}
+
+Method2(msg: "Текст сообщения");
+
+// Именованные аргументы:
+
+void Method21(string msg, int count)    // тип данных / имя метода / () принятые аргументы
+{ 
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+Method21("Текст сообщения", 4); 
+
+// Явно указываем именование аргументов
+
+void Method22(string msg, int count)    // тип данных / имя метода / () принятые аргументы
+{ 
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+Method22(count: 4, msg: "Новый текст"); 
