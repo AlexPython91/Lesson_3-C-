b7d0bc3a988938702726efa75fb2999c899b12c4
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-05 19:40:54 +0400

    added file array min/max

---
+++ array_min max/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// 1. Найти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой не сорт. позиции
+// 3. Повторять пока есть неотсортироованные элементы
+
+int[] array = {1, 5, 4, 3, 2, 6, 7, 1, 1};  // создали массив
+
+
+void PrintArray(int[] array)   // метод печати массива
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");   // вывод в строку
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)   // метод заполнения массива
+{
+    for(int i = 0; i < array.Length - 1; i++)       
+    {
+        int minPosition = i;   // определяем мин. позицию массива, запоминаем поз. рабочего эл. с ней будут действия
+
+        for(int j = i + 1; j < array.Length; j++)   //начинаем с первой поз. кот. еще не отсортирована и до последнего элемента
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int tempporary = array[i];
+        array[i] = array[minPosition];   // обмен двух значений
+        array[minPosition] = tempporary;
+    }
+}
+
+PrintArray(array);
+SelectionSort(array);
+
+PrintArray(array);
+
